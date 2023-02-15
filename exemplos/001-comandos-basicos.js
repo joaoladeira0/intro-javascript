0e886e9171ad2ce2a7cc2d3b14f385e909530760
joaoladeira0 <124743276+joaoladeira0@users.noreply.github.com>
2023-02-14 21:19:45 -0300

    Aula variáveis

---
+++ exemplos/001-comandos-basicos.js
@@ -0,0 +1,33 @@
+//comentario de 1 linha
+console.log('Hello world!');
+/*
+    comentario de mais de
+    uma linha
+*/
+
+let nome = 'Ladeira';
+let idade = 23;
+let altura = 1.73;
+let trabalha = true;
+let corFavorita = 'azul';
+let cores_favoritas = ['preto', 'branco', 'vermelho', 'roxo'];
+let endereco = {
+    'logradouro'    : 'Rua',
+    'nome_log'      : 'Marilia',
+    'n'             : '3013',
+    'bairro'        : 'centro'
+};
+let covid;
+let dengue = null;
+const exfunc = () => 10;
+
+console.log('variavel nome = ' + typeof(nome));
+console.log('variavel idade = ' + typeof(idade));
+console.log('variavel altura = ' + typeof(altura));
+console.log('variavel trabalha = ' + typeof(trabalha));
+console.log('variavel corFavorita = ' + typeof(corFavorita));
+console.log('variavel cores_favoritas = ' + typeof(cores_favoritas));
+console.log('variavel endereco = ' + typeof(endereco));
+console.log('variavel covid = ' + typeof(covid));
+console.log('variavel dengue = ' + typeof(dengue));
+console.log('variavel exfunc = ' + typeof(exfunc));
